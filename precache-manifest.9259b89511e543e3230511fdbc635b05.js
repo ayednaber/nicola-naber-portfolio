fa2c8eeab9ed385aafdaa46328ef50bf4c31b296
Ayed Naber <naberayed@gmail.com>
2023-07-29 09:14:42 -0400

    Updates

---
+++ precache-manifest.9259b89511e543e3230511fdbc635b05.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2ac989a81bc1ea75d0807d29599b13d",
+    "url": "/nicola-naber-portfolio/index.html"
+  },
+  {
+    "revision": "2442943d403a283d10ce",
+    "url": "/nicola-naber-portfolio/static/css/main.b1d7903c.chunk.css"
+  },
+  {
+    "revision": "b3d5cba22dfc3369ee3d",
+    "url": "/nicola-naber-portfolio/static/js/2.be8ead56.chunk.js"
+  },
+  {
+    "revision": "1e4333c3adde2a511ddef0add373dc0a",
+    "url": "/nicola-naber-portfolio/static/js/2.be8ead56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2442943d403a283d10ce",
+    "url": "/nicola-naber-portfolio/static/js/main.b1826320.chunk.js"
+  },
+  {
+    "revision": "fb56374b0519aa7dbb36",
+    "url": "/nicola-naber-portfolio/static/js/runtime-main.3904167f.js"
+  },
+  {
+    "revision": "d2c75079ddb682772a9274d26d267ee7",
+    "url": "/nicola-naber-portfolio/static/media/_DSC3622.d2c75079.jpg"
+  },
+  {
+    "revision": "5a082416cdbeaa8d9e569d95f22e9bea",
+    "url": "/nicola-naber-portfolio/static/media/_DSC3623.5a082416.jpg"
+  },
+  {
+    "revision": "e9f77d8e87bbfdd97c20fc74b82226b1",
+    "url": "/nicola-naber-portfolio/static/media/_DSC3629.e9f77d8e.jpg"
+  },
+  {
+    "revision": "0a8a6ed7d5485f486c37f74d3d472de3",
+    "url": "/nicola-naber-portfolio/static/media/_DSC3632.0a8a6ed7.jpg"
+  },
+  {
+    "revision": "2de3ec49b17a1698992039ec5f7a1cb6",
+    "url": "/nicola-naber-portfolio/static/media/_DSC3643.2de3ec49.jpg"
+  },
+  {
+    "revision": "37c849341a6becda8a76f1fbbf18266d",
+    "url": "/nicola-naber-portfolio/static/media/_DSC3671.37c84934.jpg"
+  },
+  {
+    "revision": "cf3fdb245ea3fd22250c9413a7bb10b3",
+    "url": "/nicola-naber-portfolio/static/media/_DSC3681.cf3fdb24.jpg"
+  },
+  {
+    "revision": "44c5a97381e44e9148cab7eeecbfb34d",
+    "url": "/nicola-naber-portfolio/static/media/_DSC3688.44c5a973.jpg"
+  },
+  {
+    "revision": "8e83b34943677be733d0d46fa686b28f",
+    "url": "/nicola-naber-portfolio/static/media/_DSC3689.8e83b349.jpg"
+  },
+  {
+    "revision": "02e08bedce5cbac48902b4a99c4850e4",
+    "url": "/nicola-naber-portfolio/static/media/_DSC3692.02e08bed.jpg"
+  },
+  {
+    "revision": "03be9063e3d6451cc63d98b4aedc25ae",
+    "url": "/nicola-naber-portfolio/static/media/_DSC3696.03be9063.jpg"
+  },
+  {
+    "revision": "b4ae5604ffe0719d467646847f1ced0a",
+    "url": "/nicola-naber-portfolio/static/media/_DSC3698.b4ae5604.jpg"
+  },
+  {
+    "revision": "2e4f021f50034e03ac88a2bff1ee0e32",
+    "url": "/nicola-naber-portfolio/static/media/_DSC3700.2e4f021f.jpg"
+  },
+  {
+    "revision": "881ab7afab48309a0accb34aaabdba64",
+    "url": "/nicola-naber-portfolio/static/media/_DSC3701.881ab7af.jpg"
+  },
+  {
+    "revision": "5829c773328f1653b828bbc007846a50",
+    "url": "/nicola-naber-portfolio/static/media/_DSC3704.5829c773.jpg"
+  },
+  {
+    "revision": "c776245596ea91ba874aeeed525f0ddf",
+    "url": "/nicola-naber-portfolio/static/media/_DSC3710.c7762455.jpg"
+  },
+  {
+    "revision": "6b82c2a0bc7d15509dbf740dea10ff5a",
+    "url": "/nicola-naber-portfolio/static/media/_DSC3722.6b82c2a0.jpg"
+  },
+  {
+    "revision": "5e95160360f850a448eb380db232d93d",
+    "url": "/nicola-naber-portfolio/static/media/_DSC3728.5e951603.jpg"
+  },
+  {
+    "revision": "d1837e1e26c783e1ce811bb6c3141691",
+    "url": "/nicola-naber-portfolio/static/media/front.d1837e1e.jpg"
+  },
+  {
+    "revision": "9e35731adea05f7a0118f493ccd87552",
+    "url": "/nicola-naber-portfolio/static/media/model-man.9e35731a.jpg"
+  },
+  {
+    "revision": "8ba7516ee21d9b69c40cb4ef0ea02c0b",
+    "url": "/nicola-naber-portfolio/static/media/model-woman-2.8ba7516e.jpg"
+  }
+]);
