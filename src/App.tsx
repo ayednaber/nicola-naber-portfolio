ca74bc141b635efafec4271f6697b3d567855ca6
Ayed Naber <nabera@mcmaster.ca>
2023-09-24 20:13:22 -0400

    Added rest of images from photoshoot 2

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,19 @@ import picture6 from "./assets/images/image00019.jpeg";
 import picture7 from "./assets/images/image00023.jpeg";
 import picture8 from "./assets/images/image00024.jpeg";
 import picture9 from "./assets/images/image00026.jpeg";
-import picture10 from "./assets/images/image00036.jpeg";
+import picture10 from "./assets/images/image00046.jpeg";
+import picture11 from "./assets/images/image00047.jpeg";
+import picture12 from "./assets/images/image00050.jpeg";
+import picture13 from "./assets/images/image00055.jpeg";
+import picture14 from "./assets/images/image00068.jpeg";
+import picture15 from "./assets/images/image00069.jpeg";
+import picture16 from "./assets/images/image00081.jpeg";
+import picture17 from "./assets/images/image00082.jpeg";
+import picture18 from "./assets/images/image00084.jpeg";
+import picture19 from "./assets/images/image00092.jpeg";
+import picture20 from "./assets/images/image00104.jpeg";
+import picture21 from "./assets/images/image00113.jpeg";
+import picture22 from "./assets/images/image00119.jpeg";
 import ModelMeasurements from "./components/Measurements/Measurements";
 
 function App() {
@@ -41,7 +53,8 @@ function App() {
       <ModelMeasurements height={185} chest={99.6} abdomen={87.9} hip={100.2} />
       <ImageGallery title="Photoshoot 1" images={[image1,image2,image3,image4,image5,image6,image7,image8,image9,image10,image11,image12,image13,image14,image15,image16,image17,image18]} />
       {/* <Testimonials /> */}
-      <ImageGallery title="Photoshoot 2" images={[picture1, picture2, picture3, picture4, picture5, picture6, picture7, picture10, picture9, picture8]} />
+      <ImageGallery title="Photoshoot 2" images={[picture1, picture2, picture3, picture4, picture5, picture6, picture7, picture10, picture9, picture8,
+      picture11,picture12,picture13,picture14,picture15,picture16,picture17,picture18,picture19,picture20,picture21,picture22]} />
       <Footer />
     </>
   );
